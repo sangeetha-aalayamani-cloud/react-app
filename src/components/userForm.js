f9cc3e900a925438fb1b518defc6cde635ed51a2
sangeetha-aalayamani-cloud <65112989+sangeetha-aalayamani-cloud@users.noreply.github.com>
2021-08-25 22:55:05 +0530

    Update userForm.js

--- src/components/userForm.js
+++ src/components/userForm.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 
 export function UserForm() {
-    const [userform, setUserform] = useState({ firstname: "sange" });
+    const [userForm, setUserForm] = useState({ firstname: "Sangeetha" });
     const [users, setUsers] = useState([]);
     function getAllUser() {
         if (!users.length)
@@ -15,12 +15,12 @@ export function UserForm() {
     getAllUser();
     const handleEvent = function (event) {
     console.log(event.target.value);
-        setUserform({
-           ...userform,  [event.target.name]: event.target.value 
+        setUserForm({
+           ...userForm,  [event.target.name]: event.target.value //spread operator
         })
     }
     const saveUser = function () {
-        axios.post("http://localhost:3000/users", userform)
+        axios.post("http://localhost:3000/users", userForm)
             .then(function (response) {
                 console.log(response.data);
             })
@@ -31,12 +31,12 @@ export function UserForm() {
     }
     return (//JSX
         <div>userform
-            <input value={userform.firstname} name='firstname' onChange={handleEvent}></input>
-            <input value={userform.age}  name='age' onChange={handleEvent}></input>
+            <input value={userForm.firstname} name='firstname' onChange={handleEvent}></input>
+            <input value={userForm.age}  name='age' onChange={handleEvent}></input>
 
             <button onClick={saveUser}> Save</button>
             {users.map(function (user, index) {
-                return <div>{user.firstname}, {user.age}</div>
+                return <div>{user.firstname}, {user.age}</div> 
             })}
         </div>
     );
