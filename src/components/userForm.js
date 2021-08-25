af55ece1b715365a1e7dd718ba2d338fd8cedeb1
sangeetha-aalayamani-cloud <65112989+sangeetha-aalayamani-cloud@users.noreply.github.com>
2021-08-25 22:06:14 +0530

    Add age in the  list

--- src/components/userForm.js
+++ src/components/userForm.js
@@ -1,22 +1,43 @@
+import axios from "axios";
 import { useState } from "react";
 
 export function UserForm() {
-    const[userform,setUserform] = useState({
-        firstname:"sange"
-    });
-    const handleEvent = function(event) {
-        console.log(event.target.value);
-        setUserform({
-            firstname: event.target.value
-        });
+    const [userform, setUserform] = useState({ firstname: "sange" });
+    const [users, setUsers] = useState([]);
+    function getAllUser() {
+        if (!users.length)
+            axios.get("http://localhost:3000/users")
+                .then(function (response) {
+                    console.log(response.data);
+                    setUsers(response.data)
+                })
     }
- return  (
-     
-    <div>Name:
-     <input value= {userform.firstname}
-     onChange= {handleEvent}></input>
-    </div>
-    
- );
-    
+    getAllUser();
+    const handleEvent = function (event) {
+    console.log(event.target.value);
+        setUserform({
+           ...userform,  [event.target.name]: event.target.value 
+        })
+    }
+    const saveUser = function () {
+        axios.post("http://localhost:3000/users", userform)
+            .then(function (response) {
+                console.log(response.data);
+            })
+            .catch(function (error) {
+                alert(error);
+            });
+        console.log('clicked...');
+    }
+    return (//JSX
+        <div>userform
+            <input value={userform.firstname} name='firstname' onChange={handleEvent}></input>
+            <input value={userform.age}  name='age' onChange={handleEvent}></input>
+
+            <button onClick={saveUser}> Save</button>
+            {users.map(function (user, index) {
+                return <div>{user.firstname}, {user.age}</div>
+            })}
+        </div>
+    );
 }
