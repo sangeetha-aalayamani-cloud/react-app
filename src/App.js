491857115a6fc7ab6f3edc8ef3a258d29dbaaa9b
sangeetha-aalayamani-cloud <65112989+sangeetha-aalayamani-cloud@users.noreply.github.com>
2021-08-25 22:55:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { Userform } from './components/userform';
+import { UserForm } from './components/userForm';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-           React - Welcome to e-Learning
+		<a href="#">
+          Welcome to React e-Learning
         </a>
-      </header> 
-      <UserForm></UserForm>
+      </header>
+	   <UserForm></UserForm>
     </div>
   );
 }
-
 export default App;
