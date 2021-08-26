100cf18a3c715977c940c4f5f464971e0e2c0f0d
sangeetha-aalayamani-cloud <65112989+sangeetha-aalayamani-cloud@users.noreply.github.com>
2021-08-26 16:32:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import { UserForm } from './components/userForm';
+import { Link, Route,  BrowserRouter as Router, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header>
-		<a href="#">
-          Welcome to React e-Learning
-        </a>
-      </header>
-	   <UserForm></UserForm>
+       <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/users">
+              <UserForm />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
