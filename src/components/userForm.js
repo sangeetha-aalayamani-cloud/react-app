aa872676ccf3c208fe2176a774ee9e0c02c70bdc
sangeetha-aalayamani-cloud <65112989+sangeetha-aalayamani-cloud@users.noreply.github.com>
2021-08-26 16:08:56 +0530

    Update userForm.js

--- src/components/userForm.js
+++ src/components/userForm.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { useState } from "react";
+import { Counter } from "./Counter";
 
 export function UserForm() {
+    const BASE_URL = "http://localhost:3001/users/";
     const [userForm, setUserForm] = useState({ firstname: "Sangeetha" });
     const [users, setUsers] = useState([]);
     function getAllUser() {
         if (!users.length)
-            axios.get("http://localhost:3000/users")
+            axios.get(BASE_URL)
                 .then(function (response) {
                     console.log(response.data);
                     setUsers(response.data)
@@ -20,7 +22,7 @@ export function UserForm() {
         })
     }
     const saveUser = function () {
-        axios.post("http://localhost:3000/users", userForm)
+        axios.post(BASE_URL, userForm)
             .then(function (response) {
                 console.log(response.data);
             })
@@ -29,11 +31,34 @@ export function UserForm() {
             });
         console.log('clicked...');
     }
+    function deleteUser(event) {
+        if (!window.confirm("Are you sure?"))
+            return;
+        axios.delete(BASE_URL + event.target.id)
+        .then(function (response) {
+            getAllUser();
+        })
+    }
+    
+    const handleEvent = function (event) {
+        console.log(event.target.value);
+        setUserForm({
+            ...userForm, [event.target.name]: event.target.value 
+        })
+    }
+ 
+    function validate(){//returns false if data is invalid
+       return ( userForm.firstname ||userForm.firstname.trim() !=='')
+    }
+    
     return (//JSX
         <div>userform
             <input value={userForm.firstname} name='firstname' onChange={handleEvent}></input>
             <input value={userForm.age}  name='age' onChange={handleEvent}></input>
-
+            <input value={userForm.firstname} name='firstname' onChange={handleEvent}></input>
+            <input value={userForm.age} name='age' onChange={handleEvent}></input>
+            <input value={userForm.gender} name='gender' onChange={handleEvent} type='radio' value='Female'/>Female
+            <input value={userForm.gender} name='gender' onChange={handleEvent} type='radio' value='Male'/>Male
             <button onClick={saveUser}> Save</button>
             {users.map(function (user, index) {
                 return <div>{user.firstname}, {user.age}</div> 
